e58634b2727329ebb8034602ed9b7c37ca62a159
codersunil-28 <tsunil28k@gmail.com>
2022-08-11 10:06:46 +0800

    Worked on files: DailyScrum.swift and ScrumdingerApp.swift to pass a binding into the List View. Used @State to create a source of truth for a value type, and used @Binding to share write access to a state with other views.

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -9,11 +9,16 @@ import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
+    /*
+     We’ll create the source of truth for our app’s data by adding a @State property to ScrumdingerApp.
+     Then, we’ll pass a binding to that data down the hierarchy to the list view.
+     */
+    @State private var scrums = DailyScrum.sampleData
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                            ScrumsView(scrums: DailyScrum.sampleData)
-                        }
+                ScrumsView(scrums: $scrums)
+            }
         }
     }
 }
